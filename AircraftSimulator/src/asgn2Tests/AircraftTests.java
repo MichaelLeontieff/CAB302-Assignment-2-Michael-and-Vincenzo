34e1b6450f09e6e3feeb0810e2e57066b2f7dc79
Michael Leontieff-Smith <michael@leontieff.net>
2016-05-11 10:45:25 +1000

    Added absent asgn2Tests package and created a Junit test class for Aircraft.java with method comments outlining each method that needs to be tested within aircraft

---
+++ AircraftSimulator/src/asgn2Tests/AircraftTests.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package asgn2Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author michaelleontieff
+ *
+ */
+public class AircraftTests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	// CONSTRUCTOR TESTS
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	// CANCEL BOOKING TESTS
+	
+	// CONFIRM BOOKING TESTS
+	
+	// FLIGHT EMPTY TESTS
+	
+	// FLIGHT FULL TESTS
+	
+	// FLY PASSENGERS TESTS
+	
+	// GET BOOKINGS TESTS
+	
+	// GET NUM BUSINESS TESTS
+	
+	// GET NUM ECONOMY TESTS
+	
+	// GET NUM FIRST TESTS
+	
+	// GET NUM PASSENGERS TESTS
+	
+	// GET NUM PREMIUM TESTS
+	
+	// GET PASSENGERS TESTS
+	
+	// HAS PASSENGER TESTS
+	
+	// SEATS AVAILABLE TESTS
+	
+	// UPGRADE BOOKING TESTS
+	
+	
+	
+	
+
+}
