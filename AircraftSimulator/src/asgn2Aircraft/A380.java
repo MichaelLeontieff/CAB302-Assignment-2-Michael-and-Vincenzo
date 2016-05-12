a6264db44dd2bab4bf7c86668683c4f1fe99f706
Vinnie Iandolo <vincenzo.iandolo@student.qut.edu.au>
2016-05-12 15:20:43 +1000

    Added Constructor to the A380.java class

--- AircraftSimulator/src/asgn2Aircraft/A380.java
+++ AircraftSimulator/src/asgn2Aircraft/A380.java
@@ -31,7 +31,7 @@ public class A380 extends Aircraft {
 	 * See {@link asgn2Aircraft.Aircraft#Aircraft(String, int, int, int, int, int)}
 	 */
 	public A380(String flightCode,int departureTime) throws AircraftException {
-		super(flightCode, departureTime, FIRST, BUSINESS, PREMIUM, ECONOMY);
+		
 		this.type = "A380";
 	}
 
