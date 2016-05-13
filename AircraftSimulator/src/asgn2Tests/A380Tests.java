00468315aafd709c4eb75405b02cbff4e80175bd
Michael Leontieff-Smith <michael@leontieff.net>
2016-05-13 12:08:00 +1000

    Completed preliminary testing for CancelBooking(), ConfirmBooking(), FlightEmpty(), FlightFull(), FlyPassenger() and the getters for protected fields of the Aircraft->A380 classes

--- AircraftSimulator/src/asgn2Tests/A380Tests.java
+++ AircraftSimulator/src/asgn2Tests/A380Tests.java
@@ -5,6 +5,8 @@ package asgn2Tests;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -12,9 +14,12 @@ import org.junit.Test;
 import asgn2Aircraft.A380;
 import asgn2Aircraft.Aircraft;
 import asgn2Aircraft.AircraftException;
+import asgn2Passengers.Business;
 import asgn2Passengers.Economy;
+import asgn2Passengers.First;
 import asgn2Passengers.Passenger;
 import asgn2Passengers.PassengerException;
+import asgn2Passengers.Premium;
 
 /**
  * @author michaelleontieff
@@ -29,15 +34,32 @@ public class A380Tests {
 	
 	// constants
 	private final String testFlightCode = "LAX123";
-	private final int testDepartTime = 11;
+	private final int testBookingTime = 5;
+	private final int testDepartTime = 20;
+	private final int testConfirmTime = 10;
 	
 	// test objects
 	private Aircraft fieldTestObject;
 	private Aircraft exceptionTestObject;
+	private Aircraft cancelBookingTest;
+	private Aircraft generalTester;
+	private Aircraft getTestNumPassClass;
 	private Passenger testPassenger;
 	
 	@Before
 	public void setUp() throws Exception {
+		// for getter testing, create aircraft and populate it with one of each passenger
+		getTestNumPassClass = new A380(testFlightCode, testDepartTime, 1, 1, 1, 1 );
+		
+		Passenger FirstPassenger = new First(testBookingTime, testDepartTime);
+		Passenger BusinessPassenger = new Business(testBookingTime, testDepartTime);
+		Passenger PremEconPassenger = new Premium(testBookingTime, testDepartTime);
+		Passenger EconPassenger = new Economy(testBookingTime, testDepartTime);
+		
+		getTestNumPassClass.confirmBooking(FirstPassenger, testConfirmTime);
+		getTestNumPassClass.confirmBooking(BusinessPassenger, testConfirmTime);
+		getTestNumPassClass.confirmBooking(PremEconPassenger, testConfirmTime);
+		getTestNumPassClass.confirmBooking(EconPassenger, testConfirmTime);
 		
 	}
 	
@@ -88,86 +110,189 @@ public class A380Tests {
 	// CANCEL BOOKING TESTS
 	
 	@Test
-	public void CancelBookingTest() {
-		fail("Not yet implemented");
+	public void CancelBookingTestCheckPassengerCount() throws PassengerException, AircraftException {
+		// create aircraft
+		cancelBookingTest = new A380(testFlightCode, testDepartTime, 1, 1, 1, 1 );
+		
+		// create passenger in confirmed state
+		testPassenger = new First(testBookingTime, testDepartTime);
+		testPassenger.confirmSeat(testBookingTime, testDepartTime);
+		
+		// add passenger to aircraft
+		cancelBookingTest.confirmBooking(testPassenger, 10);	
+		
+		// cancel booking
+		cancelBookingTest.cancelBooking(testPassenger, 10);
+		
+		assertTrue(cancelBookingTest.getNumPassengers() == 0);
 	}
 	
-	@Test (expected = PassengerException.class)
-	public void PassengerNotConfirmedExceptionTest() throws PassengerException {
-		fail("Not yet implemented");
+	@Test
+	public void CancelBookingTestCheckForPassengerObject() throws PassengerException, AircraftException {
+		// create aircraft
+		cancelBookingTest = new A380(testFlightCode, testDepartTime, 1, 1, 1, 1 );
+		
+		// create passenger in confirmed state
+		testPassenger = new First(testBookingTime, testDepartTime);
+		testPassenger.confirmSeat(testBookingTime, testDepartTime);
+		
+		// add passenger to aircraft
+		cancelBookingTest.confirmBooking(testPassenger, 10);	
+		
+		// cancel booking
+		cancelBookingTest.cancelBooking(testPassenger, 10);
+		
+		assertFalse(cancelBookingTest.getPassengers().contains(cancelBookingTest));
 	}
 	
-	@Test (expected = PassengerException.class)
-	public void InvalidCancelTimeExceptionTest() {
-		fail("Not yet implemented");
-	}
 	
 	@Test (expected = AircraftException.class)
-	public void PassengerNotInSeatingExceptionTest() {
-		fail("Not yet implemented");
+	public void PassengerNotInSeatingExceptionTest() throws AircraftException, PassengerException {
+		// create aircraft
+		cancelBookingTest = new A380(testFlightCode, testDepartTime, 1, 1, 1, 1 );
+				
+		// create passenger in confirmed state
+		testPassenger = new First(testBookingTime, testDepartTime);
+		testPassenger.confirmSeat(testBookingTime, testDepartTime);
+				
+		// DO NOT ADD TO AIRCRAFT SEATING
+		
+		// cancel booking
+		cancelBookingTest.cancelBooking(testPassenger, 10);
 	}
 	
 	// CONFIRM BOOKING TESTS
 	
-	@Test 
-	public void ConfirmBookingTest() {
-		fail("Not yet implemented");
+	@Test
+	public void ConfirmBookingTestCheckObject() throws PassengerException, AircraftException {
+		// create aircraft
+		generalTester = new A380(testFlightCode, testDepartTime, 1, 1, 1, 1 );
+		
+		// create passenger in confirmed state
+		testPassenger = new First(testBookingTime, testDepartTime);
+		testPassenger.confirmSeat(testBookingTime, testDepartTime);
+		
+		// add passenger to aircraft
+		generalTester.confirmBooking(testPassenger, 10);	
+		
+		// check for containment
+		assertTrue(cancelBookingTest.getPassengers().contains(generalTester));
 	}
 	
-	@Test (expected = PassengerException.class)
-	public void IncorrectPassengerStateExceptionTest() {
-		fail("Not yet implemented");
+	@Test
+	public void ConfirmBookingTestCheckPassengerCount() throws PassengerException, AircraftException {
+		// create aircraft
+		generalTester = new A380(testFlightCode, testDepartTime, 1, 1, 1, 1 );
+				
+		// create passenger in confirmed state
+		testPassenger = new First(testBookingTime, testDepartTime);
+		testPassenger.confirmSeat(testBookingTime, testDepartTime);
+				
+		// add passenger to aircraft
+		generalTester.confirmBooking(testPassenger, 10);	
+				
+		// check for containment
+		assertTrue(cancelBookingTest.getNumPassengers() == 1);
 	}
 	
-	@Test (expected = PassengerException.class)
-	public void InvalidConfirmTimeExceptionTest() {
-		fail("Not yet implemented");
-	}
-	
-	@Test (expected = PassengerException.class)
-	public void InvalidDepartTimeExceptionTest() {
-		fail("Not yet implemented");
+	@Test
+	public void ConfirmBookingTestCheckFirstClassPassengerCount() throws PassengerException, AircraftException {
+		// create aircraft
+		generalTester = new A380(testFlightCode, testDepartTime, 1, 1, 1, 1 );
+				
+		// create passenger in confirmed state
+		testPassenger = new First(testBookingTime, testDepartTime);
+		testPassenger.confirmSeat(testBookingTime, testDepartTime);
+				
+		// add passenger to aircraft
+		generalTester.confirmBooking(testPassenger, 10);	
+				
+		// check for containment
+		assertTrue(cancelBookingTest.getNumFirst() == 1);
 	}
 	
 	@Test (expected = AircraftException.class)
-	public void NoSeatsAvailableExceptionTest() {
-		fail("Not yet implemented");
+	public void NoSeatsAvailableExceptionTest() throws AircraftException, PassengerException {
+		// create aircraft with no seats (it had one job)
+		generalTester = new A380(testFlightCode, testDepartTime, 0, 0, 0, 0 );
+		
+		// create passenger in confirmed state
+		testPassenger = new First(testBookingTime, testDepartTime);
+		testPassenger.confirmSeat(testBookingTime, testDepartTime);
+		
+		// add passenger to aircraft, expecting exception
+		generalTester.confirmBooking(testPassenger, 10);	
+
 	}
 	
 	// FLIGHT EMPTY TESTS
 	
 	@Test
-	public void FlightEmptyEmptyTest() {
-		fail("Not yet implemented");
+	public void FlightEmptyEmptyTest() throws AircraftException {
+		// create aircraft that defaults to empty
+		generalTester = new A380(testFlightCode, testDepartTime, 1, 1, 1, 1 );
+		assertTrue(generalTester.flightEmpty());
 	}
 	
 	@Test
-	public void FlightEmptyNotEmptyTest() {
-		fail("Not yet implemented");
+	public void FlightEmptyNotEmptyTest() throws AircraftException, PassengerException {
+		// create aircraft
+		generalTester = new A380(testFlightCode, testDepartTime, 1, 1, 1, 1 );
+				
+		// create passenger in confirmed state
+		testPassenger = new First(testBookingTime, testDepartTime);
+		testPassenger.confirmSeat(testBookingTime, testDepartTime);
+				
+		// add passenger to aircraft
+		generalTester.confirmBooking(testPassenger, 10);	
+		
+		assertFalse(generalTester.flightEmpty());
 	}
 	
 	// FLIGHT FULL TESTS
 	
 	@Test
-	public void FlightFullFullTest() {
-		fail("Not yet implemented");
+	public void FlightFullNotFullTest() throws AircraftException {
+		// create aircraft that defaults to empty
+		generalTester = new A380(testFlightCode, testDepartTime, 1, 1, 1, 1 );
+		assertFalse(generalTester.flightFull());
 	}
 	
 	@Test
-	public void FlightFullNotFullTest() {
-		fail("Not yet implemented");
+	public void FlightFullFullTest() throws AircraftException, PassengerException {
+		// create aircraft with single available seat
+		generalTester = new A380(testFlightCode, testDepartTime, 1, 0, 0, 0 );
+				
+		// create passenger in confirmed state
+		testPassenger = new First(testBookingTime, testDepartTime);
+		testPassenger.confirmSeat(testBookingTime, testDepartTime);
+				
+		// add passenger to single available seat of aircraft
+		generalTester.confirmBooking(testPassenger, 10);	
+		
+		assertTrue(generalTester.flightFull());
 	}
+	
 	
 	// FLY PASSENGERS TESTS
 	
 	@Test
-	public void FlyPassengersCorrectStateChangeTest() {
-		fail("Not yet implemented");
-	}
-	
-	@Test (expected = PassengerException.class)
-	public void FlyPassengersIncorrectStateExceptionTest() {
-		fail("Not yet implemented");
+	public void FlyPassengersCorrectStateChangeTest() throws AircraftException, PassengerException {
+		// create aircraft
+		generalTester = new A380(testFlightCode, testDepartTime, 1, 1, 1, 1 );
+				
+		// create passenger in confirmed state
+		testPassenger = new First(testBookingTime, testDepartTime);
+		testPassenger.confirmSeat(testBookingTime, testDepartTime);
+				
+		// add passenger to aircraft
+		generalTester.confirmBooking(testPassenger, 10);	
+		
+		// fly passengers
+		generalTester.flyPassengers(15);
+				
+		// fetch first (and only) passenger in passenger list and check it's state
+		assertTrue(generalTester.getPassengers().get(0).isFlown());
 	}
 	
 	// GET BOOKINGS TESTS
@@ -181,42 +306,45 @@ public class A380Tests {
 	
 	@Test
 	public void GetNumBusinessClassPassengersTest() {
-		fail("Not yet implemented");
+		assertEquals(1, getTestNumPassClass.getNumBusiness());
 	}
 	
 	// GET NUM ECONOMY TESTS
 	
 	@Test
 	public void GetNumEcomonyClassPassengersTest() {
-		fail("Not yet implemented");
+		assertEquals(1, getTestNumPassClass.getNumEconomy());
 	}
 	
 	// GET NUM FIRST TESTS
 	
 	@Test
 	public void GetNumFirstClassPassengersTest() {
-		fail("Not yet implemented");
+		assertEquals(1, getTestNumPassClass.getNumFirst());
 	}
 	
 	// GET NUM PASSENGERS TESTS
 	
 	@Test
 	public void GetNumPassengersTest() {
-		fail("Not yet implemented");
+		assertEquals(4, getTestNumPassClass.getNumPassengers());
 	}
 	
 	// GET NUM PREMIUM TESTS
 	
 	@Test
 	public void GetNumPremiumClassPassengersTest() {
-		fail("Not yet implemented");
+		assertEquals(1, getTestNumPassClass.getNumPremium());
 	}
 	
 	// GET PASSENGERS TESTS
 	
 	@Test
 	public void GetPassengersTest() {
-		fail("Not yet implemented");
+		List<Passenger> comparisonPassengerList;
+		comparisonPassengerList.add(index, element);
+		// TODO determine indexing of passengers in seats list
+		// in order to construct comparison list
 	}
 	
 	// HAS PASSENGER TESTS
@@ -250,10 +378,6 @@ public class A380Tests {
 		fail("Not yet implemented");
 	}
 	
-	@Test (expected = PassengerException.class)
-	public void UpgradeBookingIncorrectPassengerStateExceptionTest() {
-		fail("Not yet implemented");
-	}
 	
 	
 	
