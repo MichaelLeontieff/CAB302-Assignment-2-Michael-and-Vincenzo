949e5df35525f19cf0ef995ad35c4d66c45e38c9
Vinnie Iandolo <vincenzo.iandolo@student.qut.edu.au>
2016-05-19 14:25:16 +1000

    Completed testing for constructor exceptions in BusinessTests.java

--- AircraftSimulator/src/asgn2Tests/BusinessTests.java
+++ AircraftSimulator/src/asgn2Tests/BusinessTests.java
@@ -8,14 +8,11 @@ package asgn2Tests;
 
  
 import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-
 import asgn2Passengers.Business;
+import asgn2Passengers.Passenger;
+import asgn2Passengers.PassengerException;
 
 /**
  * This class represents a JUnit test class for the Business class as part of the
@@ -26,24 +23,79 @@ import asgn2Passengers.Business;
  *
  */
 public class BusinessTests {
+	
+	// Declare constants
+	private static final int TEST_BOOKING_TIME = 5;
+	private static final int TEST_DEPARTURE_TIME = 20;
 
 	// Declare test objects 
 	private Business myPassenger;
+	private Business testPassenger;
+	private Passenger upgradedPassenger;
 	
 	/**
-	 * @throws java.lang.Exception
+	 * @throws PassengerException
 	 */
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws PassengerException {
+		// Creates a test passenger and sets parameters
+		myPassenger = new Business(TEST_BOOKING_TIME, TEST_DEPARTURE_TIME);
 	}
+	
+	// CONSTRUCTOR TESTS
 
+	/**
+	 * Test method for {@link asgn2Passengers.Business#Business()}.
+	 * @throws PassengerException
+	 */
+	@Test (expected = PassengerException.class)
+	public void testBusinessBookingTimeLessThanZeroBoundaryCase() throws PassengerException {
+		testPassenger = new Business(-1, TEST_DEPARTURE_TIME);
+	}
+	
+	@Test (expected = PassengerException.class)
+	public void testBusinessBookingTimeLessThanZero() throws PassengerException {
+		testPassenger = new Business(-5, TEST_DEPARTURE_TIME);
+	}
+	
+	@Test (expected = PassengerException.class)
+	public void testBusinessDepartureTimeEqualToZero() throws PassengerException {
+		testPassenger = new Business(TEST_BOOKING_TIME, 0);
+	}
+	
+	@Test (expected = PassengerException.class)
+	public void testBusinessDepartureTimeTimeLessThanZeroBoundaryCase() throws PassengerException {
+		testPassenger = new Business(TEST_BOOKING_TIME, -1);
+	}
+	
+	@Test (expected = PassengerException.class)
+	public void testFirstDepartureTimeLessThanZero() throws PassengerException {
+		testPassenger = new Business(TEST_BOOKING_TIME, -10);
+	}
+	
+	@Test (expected = PassengerException.class)
+	public void testBusinessDepartureTimeTimeLessThanBookingTime() throws PassengerException {
+		testPassenger = new Business(TEST_BOOKING_TIME, 2);
+	}
+	
+	@Test (expected = PassengerException.class)
+	public void testFirstBookingDepartureTimeLessThanBookingTimeLessThanZero() throws PassengerException {
+		testPassenger = new Business(TEST_BOOKING_TIME, -1);
+	}
+	
+	@Test (expected = PassengerException.class)
+	public void testFirstBookingDepartureTimeLessThanBookingTimeZeroDepartureTime() throws PassengerException {
+		testPassenger = new Business(TEST_BOOKING_TIME, 0);
+	}
+	
+	// NO SEATS MESSAGE TESTS
 
 	/**
 	 * Test method for {@link asgn2Passengers.Business#noSeatsMsg()}.
 	 */
 	@Test
 	public void testNoSeatsMsg() {
-		fail("Not yet implemented");
+		assertEquals("No seats available in Business", myPassenger.noSeatsMsg());
 	}
 
 	/**
@@ -51,22 +103,6 @@ public class BusinessTests {
 	 */
 	@Test
 	public void testUpgrade() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link asgn2Passengers.Business#Business(int, int)}.
-	 */
-	@Test
-	public void testBusinessIntInt() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link asgn2Passengers.Business#Business()}.
-	 */
-	@Test
-	public void testBusiness() {
 		fail("Not yet implemented");
 	}
 
