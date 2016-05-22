0426aab4c3dac83d3c7c160002ccc5791a8bae45
Michael Leontieff-Smith <michael@leontieff.net>
2016-05-22 18:57:49 +1000

    Created panels to store appropriate user input elements and added them to the JFrame - numerous private helper methods were also created

--- AircraftSimulator/src/asgn2Simulators/GUISimulator.java
+++ AircraftSimulator/src/asgn2Simulators/GUISimulator.java
@@ -10,7 +10,14 @@ import java.awt.BorderLayout;
 import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 /**
  * @author hogan
@@ -21,8 +28,31 @@ public class GUISimulator extends JFrame implements Runnable {
 	/*
 	 * Constants that define window dimensions
 	 */
-	public static final int WINDOW_WIDTH = 800;
-	public static final int WINDOW_HEIGHT = 600;
+	private static final int WINDOW_WIDTH = 800;
+	private static final int WINDOW_HEIGHT = 600;
+	
+	/*
+	 * JFrame Panel Declarations which will house elements of GUI
+	 */
+	private JPanel pnlSimulation;
+	private JPanel pnlFareClasses;
+	private JPanel pnlExecution;
+	
+	/*
+	 * Panel Elements for Simulation Panel
+	 */
+	private JLabel lblSimulation;
+	private JLabel lblRNGSeed;
+	
+	/*
+	 * Panel Elements for Simulation Panel
+	 */
+	private JLabel lblFareClasses;
+	
+	/*
+	 * Panel Elements for Simulation Panel
+	 */
+	private JLabel lblExecution;
 	
 	/**
 	 * @param arg0
@@ -53,10 +83,110 @@ public class GUISimulator extends JFrame implements Runnable {
 	}
 	
 	private void createGUI() {
+		// set general GUI parameters
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    setLayout(new BorderLayout());
+	    setLayout(new GridBagLayout());
+		
+		
+		// instantiate panels
+		pnlSimulation = createPanel(Color.BLUE);
+		pnlFareClasses = createPanel(Color.GREEN);
+		pnlExecution = createPanel(Color.RED);
+		
+		// instantiate elements
+		layoutSimulationPanel();
+		layoutFareClassesPanel();
+		layoutExecutionPanel();
+		
+
+		
+		// add panels
+		this.getContentPane().add(pnlSimulation, gridBagConstraints(0, 0, 2, 1, 50));
+		this.getContentPane().add(pnlFareClasses, gridBagConstraints(3, 0, 2, 1, 50));
+		this.getContentPane().add(pnlExecution, gridBagConstraints(6, 0, 2, 1, 50));
+		
+		
+		repaint();
 		setVisible(true);
+	}
+	
+	private GridBagConstraints gridBagConstraints(int x, int y, int w, int h, int paddingx) {
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.gridx = x;
+		constraints.gridy = y;
+		constraints.gridwidth = w;
+		constraints.gridheight = h;
+		constraints.ipadx = paddingx;
+		return constraints;
+	}
+	
+	private JPanel createPanel(Color c) {
+		JPanel jp = new JPanel();
+		jp.setBackground(c);
+		return jp;
+	}
+	
+	private void addToPanel(JPanel jp,Component c, GridBagConstraints constraints, int x, int y, int w, int h) {  
+	      constraints.gridx = x;
+	      constraints.gridy = y;
+	      constraints.gridwidth = w;
+	      constraints.gridheight = h;
+	      jp.add(c, constraints);
+	   }
+	
+	private void layoutSimulationPanel() {
+		GridBagLayout layout = new GridBagLayout();
+		pnlSimulation.setLayout(layout);
+		
+	    GridBagConstraints constraints = new GridBagConstraints(); 
+	    
+	    // Title Label
+	    lblSimulation = new JLabel("Simulation");
+	   
+	    // Sub Headings
+	    lblRNGSeed = new JLabel("RNG Seed");
+	    
+	    constraints.anchor = GridBagConstraints.CENTER;
+	    constraints.weightx = 100;
+	    constraints.weighty = 100;
+	    constraints.gridwidth = 2;
+	    
+	    addToPanel(pnlSimulation, lblSimulation, constraints, 0, 0, 2, 1);
+	    addToPanel(pnlSimulation, lblRNGSeed, constraints, 0, 2, 2, 1);
+	}
+	
+	private void layoutFareClassesPanel() {
+		GridBagLayout layout = new GridBagLayout();
+		pnlFareClasses.setLayout(layout);
+		
+	    GridBagConstraints constraints = new GridBagConstraints(); 
+	    
+	    // title label
+	    lblFareClasses = new JLabel("Fare Classes");
+	    
+	    constraints.anchor = GridBagConstraints.CENTER;
+	    constraints.weightx = 100;
+	    constraints.weighty = 100;
+	    constraints.gridwidth = 2;
+	    
+	    addToPanel(pnlFareClasses, lblFareClasses, constraints, 2, 2, 1, 5);   
+	}
+	
+	private void layoutExecutionPanel() {
+		GridBagLayout layout = new GridBagLayout();
+		pnlExecution.setLayout(layout);
+		
+	    GridBagConstraints constraints = new GridBagConstraints(); 
+	    
+	    lblExecution = new JLabel("Execution");
+	    
+	    constraints.anchor = GridBagConstraints.CENTER;
+	    constraints.weightx = 100;
+	    constraints.weighty = 100;
+	    constraints.gridwidth = 2;
+	    
+	    addToPanel(pnlExecution, lblExecution, constraints, 3, 2, 1, 5);   
 	}
 
 }
